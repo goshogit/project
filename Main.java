b0227bcd6f8e4eeb454cc2518209ea5d478959d1
goshogit <159141880+goshogit@users.noreply.github.com>
2024-02-11 21:08:23 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        calculateSum(20, 30, 40);
+        calculateSum(21, 7, 12.25);
+        byte b = 10;
+        int x = 21;
+        int y = 12;
+        int z = 7;
+        byte c = 12;
+        double half = 0.25;
+        printMySalary("заплата");
+        String а = "колко";
+        String b = "e";
+        String c = "месечната";
+        String d = "вноска";
+        String e = "за";
+        String f = "дадения";
+        String g = "период";
+        String h = "колко";
+        String i = "e";
+        String j = "годишната";
+        String k = "заплата";
+        printMyFirstPayment();
+
+     public class Main {
+    }public static void calculateSum(int number1, int number2, int number3) {
+        int sum = number1 / 10;
+        int sum = number2 / 10;
+        int sum = number3 / 10;
+        System.out.println(sum);
+    }
+        public class  Main{
+         public static void printMySalary() {
+        System.out.println("колко е годишната заплата ?(");
+    }
+     public class Main {
+        public static void Main(String[] args)
+        {
+            System.out.println("колко е месечната вноска за дадения период ?");
+        }
+
+
+      public class Main {
+    public static void printMyFirstPayment() {
+        int sum = x * z / y;
+        System.out.println(sum);
+    }
+    public class Main {
+    public static void calculateSum(int number1, int number2, byte c 12, double half 0.25) {
+        int sum = number1 + number2 + byte c + double half;
+        System.out.println(sum);
+    }
+
+    }
+
+
+
+
+
+
+
